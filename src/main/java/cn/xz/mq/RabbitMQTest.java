8d1792923432e26aa03f33544a01253de164c87e
xz <1187567532@qq.com>
2019-07-16 14:40:40 +0800

    RabbitMQ 消息确认机制

---
+++ src/main/java/cn/xz/mq/RabbitMQTest.java
@@ -0,0 +1,50 @@
+package cn.xz.mq;
+
+import com.rabbitmq.client.Channel;
+import org.apache.log4j.Logger;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author xz
+ * @ClassName RabbitMQTest
+ * @Description 测试消息队列
+ * @date 2019/7/16 0016 9:27
+ **/
+@Component
+@RabbitListener(queues = "test")
+public class RabbitMQTest {
+
+    Logger logger = Logger.getLogger(RabbitMQTest.class);
+
+    @RabbitHandler
+    public void test(String msg,Message message, Channel channel) throws IOException {
+        // RabbitMQ ACK消息确认机制
+        long tag = message.getMessageProperties().getDeliveryTag();
+        try{
+            logger.info("消费消息" + msg);
+            if(msg.equals("辣鸡")){
+                throw new RuntimeException();
+            }
+            logger.info("消息消费成功");
+            // false表示当前收到消息 true表示所有消费者收到了消息
+            channel.basicAck(tag,true);
+        }catch(Exception e) {
+            //丢弃这条消息
+            //channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
+            logger.error("消息消费失败");
+
+            // ack返回false 并且重新回到队列重发。 注意冥等性
+            channel.basicNack(tag,false,true);
+
+            // 拒绝消息
+            //channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+        }
+    }
+
+
+}
