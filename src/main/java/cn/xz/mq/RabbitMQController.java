864ff2f7bf44061a5c0ce1b42baa36e268920fb6
root <1187567532@qq.com>
2019-08-12 15:45:53 +0800

    RabbitMQ 测试Controller

---
+++ src/main/java/cn/xz/mq/RabbitMQController.java
@@ -0,0 +1,23 @@
+package cn.xz.mq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xz
+ * @ClassName RabbitMQController
+ * @Description
+ * @date 2019/8/2 17:05
+ **/
+@RestController
+public class RabbitMQController {
+    @Autowired
+    RabbitMQSend send;
+
+    @RequestMapping("/send/{msg}")
+    public void test(@PathVariable String msg) {
+        send.send(msg);
+    }
+}
