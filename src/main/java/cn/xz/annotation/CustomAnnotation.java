491592e8de65684b168adea34cd0b91beb5ceff9
xz <1187567532@qq.com>
2019-12-19 18:52:00 +0800

    自定义注解

--- src/main/java/cn/xz/annotation/CustomAnnotation.java
+++ src/main/java/cn/xz/annotation/CustomAnnotation.java
@@ -1,9 +1,6 @@
 package cn.xz.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author xz
@@ -12,8 +9,8 @@ import java.lang.annotation.Target;
  * @date 2019/6/13 0013 21:37
  **/
 @Target(ElementType.FIELD) // 可作用的范围
+@Documented
 @Retention(RetentionPolicy.RUNTIME) // 生命周期 运行时
-
 public @interface CustomAnnotation {
     public int id() default 1;
 
